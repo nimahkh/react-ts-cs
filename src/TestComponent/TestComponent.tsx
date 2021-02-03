4ec635a9c95b65aa25938db10006ff196b1e9a5d
Nima Habibkhoda <nima.habibkhoda@ttgint.com>
2021-02-03 16:56:45 +0300

    fix

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -14,7 +14,7 @@ const TestComponent: React.FC<TestComponentProps> = ({ theme }) =>
     className={`test-component test-component-${theme}`}
   >
     <h1 className="heading">I'm the test component</h1>
-    <h2>Made with love by Harvey {state}</h2>
+    <h2>Made with love {state}</h2>
   </div>
 )};
 
